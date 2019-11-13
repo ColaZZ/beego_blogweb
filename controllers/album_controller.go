4918b7d0eafa2fe8d5996a3e1b636e6f6a9d67db
张麟 <zlinxx@foxmail.com>
2019-11-14 03:56:38 +0800

    show album pics method

--- controllers/album_controller.go
+++ controllers/album_controller.go
@@ -1,9 +1,19 @@
 package controllers
 
+import (
+	"beego_blogweb/models"
+	"github.com/astaxie/beego/logs"
+)
+
 type AlbumController struct {
 	BaseController
 }
 
 func (c *AlbumController) Get() {
+	albums, err := models.FindAllAlbums()
+	if err != nil {
+		logs.Error(err)
+	}
+	c.Data["Album"] = albums
 	c.TplName ="album.html"
 }
